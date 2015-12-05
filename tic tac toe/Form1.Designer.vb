07f3cf2a44444ec0807636d6e1d8a8c983f69096
joshua1996 <kjw1996@hotmail.com>
2015-12-05 14:41:20 +0800

    change to github

--- tic tac toe/Form1.Designer.vb
+++ tic tac toe/Form1.Designer.vb
@@ -244,9 +244,9 @@ Partial Class Form1
         Me.Label7.AutoSize = True
         Me.Label7.Location = New System.Drawing.Point(372, 300)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.Size = New System.Drawing.Size(87, 13)
         Me.Label7.TabIndex = 17
-        Me.Label7.Text = "Label7"
+        Me.Label7.Text = "change to github"
         '
         'Timer1
         '
